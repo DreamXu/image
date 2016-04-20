69e6aada4dbe71929c6847bce3ef109d415a51cb
xujun <xujun@pset.suntec.net>
2016-04-20 12:15:43 +0800

    fix math_image error and add image_2

--- mathPic/image_2.c
+++ mathPic/image_2.c
@@ -14,17 +14,21 @@ unsigned char BL(int, int);
 
 unsigned char RD(int i, int j)
 {
-	// YOUR CODE HERE
+#define r(n)(rand()%n)
+	static char c[1024][1024];
+	return !c[i][j] ? c[i][j] = !r(999) ? r(256) : RD((i + r(2)) % 1024, (j + r(2)) % 1024) : c[i][j];
 }
 
 unsigned char GR(int i, int j)
 {
-	// YOUR CODE HERE
+	static char c[1024][1024];
+	return !c[i][j] ? c[i][j] = !r(999) ? r(256) : GR((i + r(2)) % 1024, (j + r(2)) % 1024) : c[i][j];
 }
 
 unsigned char BL(int i, int j)
 {
-	// YOUR CODE HERE
+	static char c[1024][1024];
+	return !c[i][j] ? c[i][j] = !r(999) ? r(256) : BL((i + r(2)) % 1024, (j + r(2)) % 1024) : c[i][j];
 }
 
 void pixel_write(int, int);
